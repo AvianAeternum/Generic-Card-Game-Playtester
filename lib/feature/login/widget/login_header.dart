1f7d5302093f4b1a3573985dca105acbbf32502c
cyberpwn <danielmillst@gmail.com>
2023-08-20 05:15:14 -0400

    ffffffff

--- lib/feature/login/widget/login_header.dart
+++ lib/feature/login/widget/login_header.dart
@@ -1,14 +1,6 @@
-/*
- * Copyright (c) 2022-2023. MyGuide
- *
- * MyGuide is a closed source project developed by Arcane Arts.
- * Do not copy, share, distribute or otherwise allow this source file
- * to leave hardware approved by Arcane Arts unless otherwise
- * approved by Arcane Arts.
- */
-
 import 'dart:math';
 
+import 'package:card/util/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 class LoginHeader extends StatelessWidget {
@@ -18,7 +10,7 @@ class LoginHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Flexible(
         child: Image.asset(
-      Assets.icon.path,
+      Assets.iconPng.path,
       width: min(MediaQuery.of(context).size.width,
               MediaQuery.of(context).size.height) /
           3,
